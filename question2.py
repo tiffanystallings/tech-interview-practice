51e20e9ec5c1124fa7d010c7049fbc2c3316c7b7
Tiffany <tiffany.c.stallings@gmail.com>
2017-10-26 13:01:37 -0400

    Start question 2 by planning out edge cases

---
+++ question2.py
@@ -0,0 +1,18 @@
+"""
+Question 2:
+Given a string a, find the longest palindromic
+substring contained in a. Your function definition
+should look like question2(a), and return a string.
+
+Edge Cases:
+An empty string will return itself, as will a string
+with only one character.
+A string with no palindromes will return an empty string.
+
+I plan on eliminating spaces and special characters
+in order to account for longer, more complex palindromic
+phrases like "A man, a plan, a canal: Panama."
+"""
+
+def question2(a):
+    return a
